5d4564b1b79885cb1995a982d8336de54326d480
Houssem-Chbcihib <53433393+Houssem-Chbcihib@users.noreply.github.com>
2023-10-11 01:31:53 +0300

    Update Assignment2.py

--- Assignment2.py
+++ Assignment2.py
@@ -45,18 +45,20 @@ if chart_type == "3D Scatter Plot":
     # Customize the figure's appearance
     fig.update_layout(
         scene=dict(
-            xaxis_title='Month',
-            yaxis_title='Day',
-            zaxis_title='Cases',
+            xaxis_title='<b>Month</b>',  # Bold title for X-axis
+            yaxis_title='<b>Day</b>',    # Bold title for Y-axis
+            zaxis_title='<b>Cases</b>',  # Bold title for Z-axis
             bgcolor='white',  # Set background color to white
-            xaxis=dict(title=dict(text='Month', font=dict(color='black'))),  # Black title for X-axis
-            yaxis=dict(title=dict(text='Day', font=dict(color='black'))),    # Black title for Y-axis
-            zaxis=dict(title=dict(text='Cases', font=dict(color='black')))    # Black title for Z-axis
         ),
         coloraxis=dict(
-            colorscale='YlGnRd',  # Use the YlGnRd color scale (green to red)
+            colorscale='RdYlGn_r',  # Use the reversed RdYlGn color scale (green to red)
             cmin=df['cases'].min(),  # Set the minimum value for color mapping
             cmax=df['cases'].max()   # Set the maximum value for color mapping
+        ),
+        coloraxis_colorbar=dict(
+            title='<b>Cases</b>',  # Bold title for color scale
+            titlefont=dict(color='black'),  # Black color for the title
+            tickfont=dict(color='black')    # Black color for color scale numbers
         )
     )
 
@@ -65,7 +67,6 @@ if chart_type == "3D Scatter Plot":
 
     # Show the 3D scatter plot using st.plotly_chart
     st.plotly_chart(fig)
-
 
 
 
